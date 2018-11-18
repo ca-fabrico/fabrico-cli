93598991014065b0610116e49dfd187dd44a5daf
Nicola Gallo <nicolagmt@hotmail.com>
2018-11-18 00:09:11 +0000

    Implemented tests

--- lib/core/filesystem/cli-physical-file-system.spec.ts
+++ lib/core/filesystem/cli-physical-file-system.spec.ts
@@ -5,19 +5,19 @@ import * as TypeMoq from 'typemoq';
 
 // modules
 import { CliPhysicalFileSystem } from './cli-physical-file-system';
-import { NodeFs } from './node-fs';
+import { FsExtra } from './fs-extra';
 
 const expect = chai.expect;
 
 describe('CliPhysicalFileSystem should', () => {
 
-  const nodeFsMock: TypeMoq.IMock<NodeFs> = TypeMoq.Mock.ofType<NodeFs>();
+  const fsExtraMock: TypeMoq.IMock<FsExtra> = TypeMoq.Mock.ofType<FsExtra>();
   let cliPFs: CliPhysicalFileSystem;
 
   beforeEach(() => {
     // Mock object must be thenable https://github.com/florinn/typemoq/issues/66
-    nodeFsMock.setup((x: any) => x.then).returns(() => undefined);
-    cliPFs = new CliPhysicalFileSystem(nodeFsMock.object);
+    fsExtraMock.setup((x: any) => x.then).returns(() => undefined);
+    cliPFs = new CliPhysicalFileSystem(fsExtraMock.object);
   });
 
   it('create a yaml file', async () => {
